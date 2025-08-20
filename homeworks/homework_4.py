cc6a9a4b25851ec3077f8255a1767ad0d42a9612
Erzhan-ish <isenbekoerzan1@gmail.com>
2025-08-20 23:42:09 +0600

    hw

--- homeworks/homework_4.py
+++ homeworks/homework_4.py
@@ -2,14 +2,19 @@ from datetime import datetime as dt
 from time import sleep
 
 
-def checktime_before_after(func):
+def checktime_before_after(func):     # определяем декоратор, принимающий функцию
     def wrapper(*args, **kwargs):
-        start = dt.now()
-        print(f"# функция была вызвана в {start.hour:02}:{start.minute:02}:{start.second:02} {start.day:02}/{start.month:02}/{start.year}")
-        result = func(*args, **kwargs)
-        end = dt.now()
-        print(f"# функция была закончена в {end.hour:02}:{end.minute:02}:{end.second:02} {end.day:02}/{end.month:02}/{end.year}")
-        return result
+        start = dt.now()              # фиксируем время начала выполнения
+        print(f"# функция была вызвана в {start.hour:02}:{start.minute:02}:{start.second:02} "
+              f"{start.day:02}/{start.month:02}/{start.year}")
+
+        result = func(*args, **kwargs)  # запускаем саму функцию и сохраняем результат
+
+        end = dt.now()                 # фиксируем время окончания выполнения
+        print(f"# функция была закончена в {end.hour:02}:{end.minute:02}:{end.second:02} "
+              f"{end.day:02}/{end.month:02}/{end.year}")
+
+        return result                  # возвращаем результат работы функции
     return wrapper
 
 
@@ -20,3 +25,4 @@ def hello_world():
 
 
 hello_world()
+
